df29900ec5e72f1912c3a5133dfed6166b5db3ce
Admin <Admin@ASUS-TUF-DASH-F>
2022-06-22 14:39:01 +0200

    Add project files.

---
+++ Walidacja PESELU/Walidacja PESELU.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int waga_1 (int a, int b)
+{
+    int w = 0;
+    w = a * 1;
+    return w;
+}
+
+int waga_3(int a, int b)
+{
+    int w = 0;
+    w = a * 3;
+    return w;
+}
+
+int waga_7(int a, int b)
+{
+    int w = 0;
+    w = a * 7;
+    return w;
+}
+
+int waga_9(int a, int b)
+{
+    int w = 0;
+    w = a * 9;
+    return w;
+}
+
+int main()
+{
+    string pesel;
+    const int table_size = 11;
+    int table[table_size];
+    int i;
+    
+    
+    cout << "Podaj swój numer PESEL: ";
+    
+    cin >> pesel;
+
+    
+
+
+    
+}
