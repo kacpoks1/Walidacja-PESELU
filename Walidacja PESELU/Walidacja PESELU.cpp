4d0f1f4883cb8a13c7cf5667e1fac05a2adf2ed9
kacpoks1 <abramowiczkacper66@gmail.com>
2023-09-11 22:18:36 +0200

    działa

--- Walidacja PESELU/Walidacja PESELU.cpp
+++ Walidacja PESELU/Walidacja PESELU.cpp
@@ -3,34 +3,35 @@
 
 using namespace std;
 
+int walidacja(const string& pesel, const int waga[]);
 
-int main ()
+int main()
 {
-	string pesel; 
-	cout << "Podaj numer PESEL: ";
-	cin >> pesel;
-	int waga[] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
-	int suma = 0;
-	if (walidacja(suma))
-	{
-		cout << "PESEL jest poprawny";
-	}
-	
-	else
-	{
-		cout << "PESEL nie jest poprawny";
-	}
+    string pesel;
+    cout << "Podaj numer PESEL: ";
+    cin >> pesel;
+    int waga[] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+    int suma = walidacja(pesel, waga);
 
-	return 0;
+    if (suma % 10 == 0)
+    {
+        cout << "PESEL jest poprawny" << endl;
+    }
+    else
+    {
+        cout << "PESEL nie jest poprawny" << endl;
+    }
+
+    return 0;
 }
 
-int  walidacja(string pesel, int waga, int suma)
+int walidacja(const string& pesel, const int waga[])
 {
-	suma = 0;
-	for (int i = 0; i < 11; i++)
-	{
-		suma += ((pesel[i]) - '0') * waga[i];
-	}
+    int suma = 0;
+    for (int i = 0; i < 11; i++)
+    {
+        suma += ((pesel[i]) - '0') * waga[i];
+    }
 
-	return suma % 10 == 0;
+    return suma;
 }
